6de4629f5c13309db3d825e44e692f85dc9fef59
Sean <tweestidbunny@gmail.com>
2024-08-07 08:15:47 -0400

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let cTemp = (temp - 32) * (5 / 9);
+  return +convertToFixed(cTemp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let fTemp = temp * (9 / 5) + 32;
+  return +convertToFixed(fTemp);
 };
+
+function convertToFixed(temp) {
+  console.log(String(temp)[String(temp).indexOf('.') + 1]);
+  if (String(temp)[String(temp).indexOf('.') + 1] === '0') {
+    return temp.toFixed(0);
+  } else {
+    return temp.toFixed(1);
+  }
+}
 
 // Do not edit below this line
 module.exports = {
