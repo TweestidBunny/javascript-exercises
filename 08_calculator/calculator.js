3f5c2cd870e03a022029571cf99134844efdceec
Sean <tweestidbunny@gmail.com>
2024-08-10 20:07:13 -0400

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
-	
+const add = function (...args) {
+
+  let result = args.reduce((total, current) => total + current);
+
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function (...args) {
+
+  let result = args.reduce((total, current) => total - current);
+
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+
+  let result = arr.reduce((total, current) => total + current, 0);
+
+  return result;
 };
 
-const multiply = function() {
+const multiply = function (arr) {
 
+  let result = arr.reduce((total, current) => total * current);
+
+  return result;
 };
 
-const power = function() {
-	
+const power = function (...args) {
+
+  let result = args.reduce((total, current) => total ** current);
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+
+  let result;
+  let nums = []
+
+  for (let i = 1; i <= num; i++) {
+    nums.push(i);
+  }
+
+  if (num > 0) {
+    result = nums.reduce((total, current) => total * current);
+  } else {
+    result = 1;
+  }
+
+
+  return result;
 };
 
 // Do not edit below this line
