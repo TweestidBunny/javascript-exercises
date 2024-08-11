0b0f6cb1b1865af36925c64d829af5d2809acefc
Sean <tweestidbunny@gmail.com>
2024-08-11 09:06:45 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
 
-};
+  let addAge = arr.map(item => {
+
+    let age = 0;
+
+    if (item.yearOfDeath) {
+      age = item.yearOfDeath - item.yearOfBirth;
+    } else {
+      let year = new Date().getFullYear();
+      age = year - item.yearOfBirth;
+    }
+
+    item['age'] = age;
+
+    return item;
+  });
+
+  addAge.sort((a, b) => a.age - b.age);
+
+  return addAge[addAge.length - 1];
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
