be07270444fa141822c2efd900b5b6a6593ab940
Sean <tweestidbunny@gmail.com>
2024-08-10 21:55:21 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (iteration) {
+  let start = 1;
+  let fibArr = [];
 
+  if (+iteration < 0) return 'OOPS';
+
+  if (!+iteration) return 0;
+
+  for (let i = 0; i < +iteration; i++) {
+    if (fibArr.length < 2) {
+      fibArr.push(start);
+    } else {
+      fibArr.push(fibArr[i - 1] + fibArr[i - 2]);
+    }
+  }
+
+  return fibArr[fibArr.length - 1];
 };
 
 // Do not edit below this line
