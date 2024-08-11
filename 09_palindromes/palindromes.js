22f9f7c680afbc9d7a413f03bc31e80d7de35b58
Sean <tweestidbunny@gmail.com>
2024-08-10 21:23:40 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+  let regex = /^[a-zA-Z1-9]+$/;
+  let condensedStr = [];
+
+  for (let char of str) {
+    if (regex.test(char)) {
+      condensedStr.push(char.toLowerCase());
+    }
+  }
+
+  let sliced = condensedStr.slice().reverse().join('');
+
+  return sliced === condensedStr.join('');
 };
 
 // Do not edit below this line
